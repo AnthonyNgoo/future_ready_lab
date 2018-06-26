52622e2df1fbd73676de553d8981d27e026f801a
AnthonyNgoo <timsutters@gmail.com>
2018-06-26 11:57:17 -0700

    version 2

--- Week_2/NumberGuessing_V1/main.js
+++ Week_2/NumberGuessing_V1/main.js
@@ -8,6 +8,13 @@ var from = parseInt(prompt("Enter the lower bound."));
 
 var to = parseInt(prompt("Enter the higher bound."));
 
+//Limits the number that can be entered into the bounds.1
+while (from < 0){
+    from = parseInt(prompt("Enter a number greater than 0"));
+} while (to > 1000){
+    to = parseInt(prompt("Enter a number less than 1000"))
+}
+
 //get an integer between [from, to]
 //Math.random() return decimals, used Math.round() to get whole number
 var target = Math.round(Math.random() * (to - from) + from);
@@ -17,15 +24,15 @@ var currentGuess = parseInt(prompt("Guess a number between " + from + " and " + 
 var totalGuesses =1;
 
 //loop until user guesses correct number
-while(currentGuess != target){
+while(currentGuess != target || currentGuess > 1000){
     if (currentGuess < target){
-        currentGuess = parseInt(prompt("Enter a higher number"));
-    
+        currentGuess = parseInt(prompt("Enter a higher number that is more than the lower bound"));
         totalGuesses++;
-    }else if (currentGuess > target){
-        currentGuess = parseInt(prompt("Enter a lower number"));
+        
+    }else if (currentGuess > target || currentGuess < 0){
+        currentGuess = parseInt(prompt("Enter a lower number that is less than the higher bound"));
         totalGuesses++
     }
 }
-    alert("It took " +totalGuesses + "tries to guess the correct number.");
+    alert("It took " +totalGuesses + " tries to guess the correct number.");
 }
